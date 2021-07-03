215b92154bab9f81b2eda34f60072df7af1f5ca1
NowonDev <nowondeve@gmail.com>
2021-07-03 19:36:58 +0200

    affichage du temps et barre de progression(bug)

--- js/player.js
+++ js/player.js
@@ -4,6 +4,8 @@ const widget1 = SC.Widget(iframeElement); //viens de l'api voir arboressence a g
 //const widget2 = SC.Widget(iframeElementID);
 //commande D pour selectionner
 console.log(widget1);
+const progressBar = document.querySelector("#progress");
+ 
 
 //const playButton = document.querySelector("#play")
 
@@ -28,6 +30,12 @@ widget1.bind(SC.Widget.Events.READY, () => {
     });
   });
 });
+widget1.bind(SC.Widget.Events.PLAY_PROGRESS, () => {
+ 
+  widget1.getPosition((position)=>{
+    progressBar.setAttribute("value",position);
+  });
+});
 function createTrack(sound, index) {
   console.log(sound);
   const listDiv = document.querySelector(".tracklist");
@@ -42,27 +50,23 @@ function createTrack(sound, index) {
 
   const duration = document.createElement("p");
   duration.classList.add("track-duration");
-  duration.textContent = sound.duration;
+  duration.textContent = getDisplayTime(sound.duration); //convertisseur ms en minute
   element.appendChild(duration);
 
   element.addEventListener("click", () => {
-    console.log("click", sound.title);
     widget1.getCurrentSoundIndex((currentindex) => {
-      console.log(currentindex, index);
       const diff = Math.abs(currentindex - index);
       if (index > currentindex) {
         //si celui qu'on veut choisir est > que celui qu'on est ene train de jouer
         for (let i = 0; i < diff; i++) {
           widget1.next();
-         
         }
       } else {
         for (let i = 0; i < diff; i++) {
           widget1.prev();
-          
         }
       }
-      widget1.play();
+      playMusic();
     });
   });
 
@@ -70,7 +74,46 @@ function createTrack(sound, index) {
 }
 
 function playMusic() {
-  widget1.play();
+;
+  
+  
+  widget1.getCurrentSound((sound)=>{
+    progressBar.setAttribute("value", 0);
+  progressBar.setAttribute("min", 0);
+    progressBar.setAttribute("max", sound.duration); 
+    setPosition(0)
+    widget1.play();
+    widget1.getPosition((position)=>{
+     console.log(position);
+    });
+    
+  });
+ 
+
+}
+function setPosition(ms) {
+widget1.seekTo(ms)
+}
+
+function getDisplayTime(ms) {
+  const totalSec = Math.floor(ms / 1000);
+  let hour = Math.floor(totalSec / (60 * 60));
+  let min = Math.floor(totalSec / 60) % 60;
+  let sec = totalSec % 60;
+  let str = "";
+  if (hour > 0) {
+    if (hour < 10) hour = "0" + hour;
+    str += hour + ":";
+  }
+  if (min < 10) min = "0" + min;
+  str += min + ":";
+
+  if (sec < 10) sec = "0" + sec;
+  str += sec;
+
+  return str;
+  //return [hour, min, sec].join(":");
+  //join creer une chaine de caractere avec separateur
 }
 
 /* getVolume(callback) — returns the current volume, in the range of [0, 100].
@@ -83,5 +126,3 @@ isPaused(callback) — whether the widget is paused. */
 
 /* durée, volume, barre de progression état
  */
-
-
